2bf75d696d87cf4cd072177687a20b253d6fa5a4
Daniel <danielkrich147@gmail.com>
2022-06-17 11:53:53 +0300

    Revert "remove comment" (#2)

--- TextAppApi/Startup.cs
+++ TextAppApi/Startup.cs
@@ -40,6 +40,7 @@ namespace TextAppApi
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
