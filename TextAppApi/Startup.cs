8a3dd5ffc4cc21533081e0f93ef7ccbe474add82
zogov123 <danielkrich147@gmail.com>
2022-06-17 12:06:03 +0300

    rm comment

--- TextAppApi/Startup.cs
+++ TextAppApi/Startup.cs
@@ -40,7 +40,6 @@ namespace TextAppApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -126,7 +125,6 @@ namespace TextAppApi
                 .AddAuthorization();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors(x => x
